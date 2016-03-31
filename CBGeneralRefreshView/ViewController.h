7fd83559640e6976527501cf25afbd7862cb3f66
chebao <chebao@chebaodeimac.local>
2016-03-31 14:12:55 +0800

    Initial Commit

---
+++ CBGeneralRefreshView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CBGeneralRefreshView
+//
+//  Created by chebao on 16/3/31.
+//  Copyright © 2016年 chebao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CBGeneralRefreshView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CBGeneralRefreshView
+//
+//  Created by chebao on 16/3/31.
+//  Copyright © 2016年 chebao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
