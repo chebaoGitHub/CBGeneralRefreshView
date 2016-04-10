e633470f263e9ce597548eea6641e62b1457b287
chebao <chebao@chebaodeimac.local>
2016-04-10 21:10:34 +0800

    com

---
+++ CBGeneralRefreshView/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  CBGeneralRefreshView
+//
+//  Created by chebao on 16/3/31.
+//  Copyright © 2016年 chebao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
--- CBGeneralRefreshView/ViewController.m
+++ CBGeneralRefreshView/ViewController.m
@@ -8,16 +8,61 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import "FirstViewController.h"//没有使用xib的Controller
+#import "UIScrollView+HeaderRefresh.h"
+//#import "CBRefresh.h"
+
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *table;
 
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.table.delegate = self;
+    self.table.dataSource = self;
+    [self.table registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    
+    
+    
 }
+
+
+- (void)viewDidLayoutSubviews{
+    [self.table addRefreshHeader:nil];
+//    self.table.headerView;
+}
+
+
+#pragma mark- table
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [self.table dequeueReusableCellWithIdentifier:@"cell"];
+    cell.backgroundColor = [UIColor yellowColor];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+//    FirstViewController * vc = [[FirstViewController alloc] init];
+//    [self.navigationController pushViewController:vc animated:YES];
+}
+
+
+#pragma mark- view controller
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
